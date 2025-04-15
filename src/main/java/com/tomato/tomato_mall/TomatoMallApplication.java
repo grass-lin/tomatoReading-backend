851e4e255c40189edc05b8ab37731f1aaa5d09ad
231250088-Ou Liyan <231250088@smail.nju.edu.cn>
2025-04-15 12:35:43 +0800

    fix: processExpiredOrders

--- src/main/java/com/tomato/tomato_mall/TomatoMallApplication.java
+++ src/main/java/com/tomato/tomato_mall/TomatoMallApplication.java
@@ -2,12 +2,12 @@ package com.tomato.tomato_mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling  // Add this annotation to enable scheduling
 public class TomatoMallApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TomatoMallApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TomatoMallApplication.class, args);
+    }
 }
